08453640b707056dc47eda03e4abc7dadd5a5260
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-06-20 08:58:17 +0000

    JSX and Babel

--- App.js
+++ App.js
@@ -10,38 +10,46 @@ import ReactDOM from "react-dom/client";
     </div>
 */
 
-const parent=React.createElement(
-    "div",
-    {id: "parent"},
-    React.createElement(
-        "div",
-        {id: "child"},
-        [
-            React.createElement(
-                "h1",
-                {},
-                "I'm h1 tag"
-            ),
-            React.createElement(
-                "h2",
-                {},
-                "I'm h2 tag"
-            )
-        ]
-    )
-);
+// const parent=React.createElement(
+//     "div",
+//     {id: "parent"},
+//     React.createElement(
+//         "div",
+//         {id: "child"},
+//         [
+//             React.createElement(
+//                 "h1",
+//                 {},
+//                 "I'm h1 tag"
+//             ),
+//             React.createElement(
+//                 "h2",
+//                 {},
+//                 "I'm h2 tag"
+//             )
+//         ]
+//     )
+// );
 
 
-const heading=React.createElement(
-    "h1",
-    {id: "heading"},
-    "Hello World from React!"
-);
+// const heading=React.createElement(
+//     "h1",
+//     {id: "heading"},
+//     "Hello World from React!"
+// );
 
 // console.log(heading); //heading is an object
-console.log(parent);
+// console.log(parent);
+
+
+// JSX - not HTML in JS. HTML-like or XML-like syntax. 
+// transpiled before it reaches the JS Engine. Transpiling is done by Parcel - Babel.
+// JSX is transpiled to ReactElement and then to JS object and then to html.
+const jsxHeading=<h1 className="heading">Namaste React from JSX 🚀</h1>;
+console.log(jsxHeading);
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(heading);
-root.render(parent);
+root.render(jsxHeading);
